7a6dd9c5fed166edf1991e45b373b4e2524cf34c
Kaneda-prog <59671354+Kaneda-prog@users.noreply.github.com>
2025-03-12 19:11:04 -0300

    criado main.py so de resenha e pra testar commits do vs code

---
+++ main.py
@@ -0,0 +1,24 @@
+# importando modulos
+import pygame
+import sys
+
+pygame.init()
+
+# criando display
+display = pygame.display.set_mode((500, 500))
+
+# criando image surface
+image = pygame.image.load('exemplo.png')
+
+# colocando image surface no display surface
+display.blit(image,(100,100))
+
+pygame.time.wait(5000)
+
+while True:
+  
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            sys.exit()
+    pygame.display.flip()
