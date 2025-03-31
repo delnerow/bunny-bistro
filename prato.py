b6e976495e3253b9bfa38e8b18360d8262ac50cd
Kaneda-prog <59671354+Kaneda-prog@users.noreply.github.com>
2025-03-31 10:34:02 -0300

    todo no prato

--- prato.py
+++ prato.py
@@ -1,6 +1,7 @@
 
 #Código numérico para relacionar receitas
 pratos={(1,1,0,0,0,0):"Caponata", (0,0,12,2,2,0):"hamburguer",(0,0,0,23,23,123):"quiche"}
+
 interface={1:"cortado",2:"batido",3:"assado",12:"cortado e batido",13:"cortado e assado",23:"batido e assado",123:"cortado, assado e batido"}
 # Código das receitas (T de tomate, C de cebola... / C de corte, B de bater e A de assar...)
 # TCGFLB       C B A  CB CA  ...
@@ -14,6 +15,9 @@ interface={1:"cortado",2:"batido",3:"assado",12:"cortado e batido",13:"cortado e
 # e elas atualizam o estado de tudo que tá la, ou a maquina rouba o ingrediente,
 # atualiza ele e depois retorna ao prato. adotei essa última
 # 
+
+#TODO
+#proibir de pegar mais de um ingrediente
 
 class Prato:
     def __init__(self):
