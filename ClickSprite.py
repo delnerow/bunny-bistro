76edf75ce01802d9ddb759f8f024cc0b2ce36fc5
Kaneda-prog <59671354+Kaneda-prog@users.noreply.github.com>
2025-04-15 19:16:34 -0300

    comentários de clientes, e sprites

--- ClickSprite.py
+++ ClickSprite.py
@@ -1,6 +1,6 @@
 import pygame
  
- # subclasse de pygame.Sprite
+# Sprite acionável ao clicar o mouse nele
 class ClickableSprite(pygame.sprite.Sprite):
     def __init__(self, image, x, y, callback):
         super().__init__()
@@ -9,15 +9,16 @@ class ClickableSprite(pygame.sprite.Sprite):
         self.rect.x = x
         self.rect.y = y
         self.callback = callback
-    #
     # self.rect faz um collider em torno da imagem 
     # pro point&click do mouse detectar
-    # callback é a função a ser chamada quando for clicada
-    #
+    # Callback é a função a ser chamada quando for clicada
  
     def update(self, events):
         for event in events:
             if event.type == pygame.MOUSEBUTTONUP:
                 if self.rect.collidepoint(event.pos):
                     self.callback()
+    # Atualiza em busca do evento do clique do mouse
+    # Caso tenha clicado no collider
+    # chamar callback
  
